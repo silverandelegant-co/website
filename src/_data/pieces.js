cb3c7dc232ec8ef8ac9b57abdbc866edac1abde7
hamzasari <hamza.sari@gmail.com>
2026-05-31 18:39:04 +0100

    feat: add support for image-only pieces and an explore more section on product pages

--- src/_data/pieces.js
+++ src/_data/pieces.js
@@ -3,11 +3,14 @@ const rings = require('./rings.json');
 const necklaces = require('./necklaces.json');
 const bracelets = require('./bracelets.json');
 
+const allPieces = [
+  ...(earrings.pieces || []),
+  ...(rings.pieces || []),
+  ...(necklaces.pieces || []),
+  ...(bracelets.pieces || [])
+];
+
 module.exports = {
-  pieces: [
-    ...(earrings.pieces || []),
-    ...(rings.pieces || []),
-    ...(necklaces.pieces || []),
-    ...(bracelets.pieces || [])
-  ]
+  pieces: allPieces,
+  detailedPieces: allPieces.filter(p => !p.no_detail)
 };
